016f0ac7184232a1991dd613400295325e12db67
LiawWun <119928243+LiawWun@users.noreply.github.com>
2023-10-14 03:10:07 +0800

    Update 11216_Pyramid_blueprint.c

--- midterm1/11216_Pyramid_blueprint/11216_Pyramid_blueprint.c
+++ midterm1/11216_Pyramid_blueprint/11216_Pyramid_blueprint.c
@@ -4,12 +4,12 @@ int main(){
   scanf("%d", &layer);
   length = (2 * layer) - 1;
   for (int i = 1; i <= layer; i ++){
-	  int stone_number = (2 * i) - 1;
+    int stone_number = (2 * i) - 1;
     int space_number = (length - stone_number) / 2;
-	  for (int j = 1; j <= space_number; j ++) printf("_");
-	  for (int k = 1; k <= stone_number; k ++) printf("*"); 
-	  for (int l = 1; l <= space_number; l ++) printf("_");
-	  printf("\n");
+    for (int j = 1; j <= space_number; j ++) printf("_");
+    for (int k = 1; k <= stone_number; k ++) printf("*"); 
+    for (int l = 1; l <= space_number; l ++) printf("_");
+    printf("\n");
   }
   return 0;
 }
