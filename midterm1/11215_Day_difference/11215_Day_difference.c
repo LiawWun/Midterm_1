78fe5dc4902367da39be79bf148c5fd4056871a3
LiawWun <119928243+LiawWun@users.noreply.github.com>
2023-10-14 03:07:30 +0800

    Update 11215_Day_difference.c

--- midterm1/11215_Day_difference/11215_Day_difference.c
+++ midterm1/11215_Day_difference/11215_Day_difference.c
@@ -6,16 +6,16 @@ int main(){
   scanf("%d %d %d", &year2, &month2, &day2); 
   while( !(year1 == year2 && month1 == month2 && day1 == day2)){
     month_days[2] = (year1 % 4 == 0 && year1 % 100 != 0) || (year1 % 400 == 0) ? 29 : 28;
-	day_difference ++;
-	day1 ++;
-	if(day1 > month_days[month1]){
-	  day1 = 1;
-	  month1 ++;
-	}
-	if(month1 > 12){
-	  month1 = 1;
-	  year1 ++;
-	}
+    day_difference ++;
+    day1 ++;
+    if(day1 > month_days[month1]){
+      day1 = 1;
+      month1 ++;
+    }
+    if(month1 > 12){
+      month1 = 1;
+      year1 ++;
+    }
   }
   printf("%d", day_difference);
   return 0;
